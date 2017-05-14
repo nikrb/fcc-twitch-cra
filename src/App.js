a24aa3d93c26e096d4433c0bd0bcf61bcffcec77
nikrb <nik_rb@yahoo.com>
2017-05-14 23:58:29 +0100

    layout twitch app components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import TwitchList from './TwitchList';
+import SearchBar from './SearchBar';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  state = {
+    show_status : "all"
+  };
+  handleAll = () => {
+    this.setState( { show_status: "all"});
+  };
+  handleOffline = () => {
+    this.setState( { show_status: "offline"});
+  };
+  handleOnline = () => {
+    this.setState( { show_status: "online"});
+  };
+  render = () => {
+    const buttonBar = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      backgroundColor: "red",
+      border: "2px solid rgba( 64,64,192,0.2)",
+      borderTopLeftRadius: "10px",
+      borderTopRightRadius: "10px",
+      backgroundImage: "linear-gradient( 0deg, rgba( 64,64,192,0.5), rgba( 64,64,192,0.3))"
+    };
+    const container = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      flex: "0 1 400px"
+    };
+    const list_wrapper = {
+      width: "100%",
+      backgroundColor: "white"
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div style={container}>
+        <div className="App">
+          <div style={buttonBar}>
+            <button type="button"
+              className={this.state.show_status==="all"?"button-active":""}
+              onClick={this.handleAll}>All</button>
+            <button type="button"
+              className={this.state.show_status==="offline"?"button-active":""}
+              onClick={this.handleOffline}>Offline</button>
+            <button type="button"
+              className={this.state.show_status==="online"?"button-active":""}
+              onClick={this.handleOnline}>Online</button>
+          </div>
+          <div style={list_wrapper}>
+            <SearchBar />
+            <TwitchList  />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
-  }
+  };
 }
 
 export default App;
